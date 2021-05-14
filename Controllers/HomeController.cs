011b4b3552a8fc69c01f8dbf9b3528870aa97299
Luiz Alfredo Campana Junior <luiz.jr831@gmail.com>
2021-05-14 08:41:28 -0300

    não lembro

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,36 +18,120 @@ namespace PokeGraf.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(string searchString)
+        public IActionResult Index(string busca)
         {
-            if (String.IsNullOrEmpty(searchString))
+            if (String.IsNullOrEmpty(busca))
             {
                 return View();
             }
             else
             {
-                List<Pokemon> pokemons = new List<Pokemon>(3);
+                /**
+                if (achou no banco?)
+                {
 
-                RestSharp.RestClient pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/pokemon/ditto");
-                RestSharp.RestRequest pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
-                var resultado = pokeApiCliente.Execute(pokeApiRequest);
-                Models.Pokemon pokemon = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(resultado.Content);
-                pokemons.Add(pokemon);
+                }
+                else
+                {
+                  busca na api
+                }
+                */
 
-                pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/pokemon/bulbasaur");
-                pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
-                resultado = pokeApiCliente.Execute(pokeApiRequest);
-                pokemon = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(resultado.Content);
-                pokemons.Add(pokemon);
+                RestSharp.RestClient pokeApiCliente;
+                RestSharp.RestRequest pokeApiRequest;
 
-                pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/pokemon/pikachu");
-                pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
-                resultado = pokeApiCliente.Execute(pokeApiRequest);
-                pokemon = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(resultado.Content);
-                pokemons.Add(pokemon);
+                for (int i = 0; i < 19; i++)
+                {
+                    pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/type/{i}/");
+                    pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                    var resultado = pokeApiCliente.Execute(pokeApiRequest);
+                    if (resultado.IsSuccessful)
+                    {
+                        Models.Type tipo = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Type>(resultado.Content);
 
-                ViewBag.Pokemons = pokemons;
+                        foreach (var item in tipo.Pokemon)
+                        {
+                            pokeApiCliente = new RestSharp.RestClient(item.Pokemon.Url);
+                            pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                            resultado = pokeApiCliente.Execute(pokeApiRequest);
+                            Pokemon poke = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Pokemon>(resultado.Content);
 
+                            //Inserir no banco as infos do pokemon
+                            string nome = poke.Name;
+                            int peso = poke.Weight;
+                            int altura = poke.Height;
+                            string pokeElemento;
+                            
+                            foreach (var tipoo in poke.Types)
+                            {
+                                pokeElemento = tipoo.Type.Name;
+                            }
+                            string sprite = poke.Sprites.FrontDefault;
+
+
+                            pokeApiCliente = new RestSharp.RestClient(poke.Species.Url);
+                            pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                            resultado = pokeApiCliente.Execute(pokeApiRequest);
+                            PokemonSpecies pokeS = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.PokemonSpecies>(resultado.Content);
+                            
+                            //resto das infos
+                            int idPoke = pokeS.Order;
+                            string descricao = pokeS.FlavorTextEntries[0].FlavorText;
+                            string raridade;
+                            if (pokeS.IsBaby)
+                            {
+                                raridade = "Baby";
+                            }
+                            else if (pokeS.IsLegendary)
+                            {
+                                raridade = "Legendary";
+                            }
+                            else if (pokeS.IsMythical)
+                            {
+                                raridade = "Mythical";
+                            }
+                            else
+                            {
+                                raridade = "Commom";
+                            }
+                        }
+                    }
+                }
+
+                //RestSharp.RestClient pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/pokemon/{busca}/");
+                //RestSharp.RestRequest pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                //var resultado = pokeApiCliente.Execute(pokeApiRequest);
+                //Pokemon pokemon;
+                /**
+                if (resultado.IsSuccessful)
+                {
+                    pokemon = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(resultado.Content);
+                    ViewBag.Pokemon = pokemon;
+
+                    pokeApiCliente = new RestSharp.RestClient($"https://pokeapi.co/api/v2/pokemon-species/{pokemon.Id}/");
+                    pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                    resultado = pokeApiCliente.Execute(pokeApiRequest);
+                    PokemonSpecies pokemonS = Newtonsoft.Json.JsonConvert.DeserializeObject<PokemonSpecies>(resultado.Content);
+                    ViewBag.PokemonS = pokemonS;
+
+                    pokeApiCliente = new RestSharp.RestClient(pokemonS.EvolutionChain.Url);
+                    pokeApiRequest = new RestSharp.RestRequest(RestSharp.Method.GET);
+                    resultado = pokeApiCliente.Execute(pokeApiRequest);
+                    EvolutionChain pokemonChain = Newtonsoft.Json.JsonConvert.DeserializeObject<EvolutionChain>(resultado.Content);
+                    ViewBag.PokemonChain = pokemonChain;
+
+                    /**
+                     * chain list = 0 quando não tem evolução
+                     * */
+                    Console.WriteLine("");/**
+                }
+                else
+                {
+                    pokemon = new Pokemon();
+                    pokemon.Name = "NãoExiste";
+                    ViewBag.Pokemon = pokemon;
+                }
+                */
                 return View();
             }
         }
@@ -68,12 +152,12 @@ namespace PokeGraf.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        
+
 
         //[HttpPost]
         //public void Testando()
         //{
-        
+
 
         //    Response.Redirect("/resultado");
         //}
--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1072,6 +1072,12 @@ namespace PokeGraf.Models
         [JsonProperty("is_baby")]
         public bool IsBaby { get; set; }
 
+        [JsonProperty("is_legendary")]
+        public bool IsLegendary { get; set; }
+
+        [JsonProperty("is_mythical")]
+        public bool IsMythical { get; set; }
+
         /// <summary>
         /// Initial hatch counter: one must walk 255 × (hatch_counter + 1) steps
         /// before this Pokémon's egg hatches, unless utilizing bonuses like
